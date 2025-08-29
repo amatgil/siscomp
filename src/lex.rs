6abc0fabd1d29d82b529a572d9b724abf3025f30
amatgil <amatgilvinyes@gmail.com>
2025-08-29 18:19:46 +0200

    Lexing!

--- src/lex.rs
+++ src/lex.rs
@@ -1,4 +1,4 @@
-use std::str::{Chars, FromStr};
+use std::str::FromStr;
 
 struct Lexer<'src> {
     whole: &'src str,
@@ -29,6 +29,7 @@ pub enum Token<'src> {
     PercentEqual,
     Semicolon,
     Tilde, // one's complement!
+    TildeEqual,
     BangEqual,
     EqualEqual,
     LessEqual,
@@ -37,9 +38,13 @@ pub enum Token<'src> {
     Greater,
     Bang,
     Equal,
+    Ampersand,
+    DoubleAmpersand,
+    Pipe,
+    DoublePipe,
     String(&'src str),
     Keyword(Keyword), // I don't know if they should just be a straight ident...
-    Ident,
+    Ident(&'src str),
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -141,17 +146,18 @@ impl<'src> Iterator for Lexer<'src> {
     type Item = Token<'src>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        fn ret_and_adv<'src>(s: &mut Lexer<'src>, len: usize, t: Token<'src>) -> Token<'src> {
-            s.byte_pos += len;
+        use Token as T;
+        fn ret_and_adv<'src>(s: &mut Lexer<'src>, left: char, t: Token<'src>) -> Token<'src> {
+            s.byte_pos += left.len_utf8();
             t
         }
 
         /// Is 'left' followed by 'right'?
         fn followed_by<'src>(
             s: &mut Lexer<'src>,
+            next: Option<char>,
             left: char,
             right: char,
-            next: Option<char>,
             yes: Token<'src>,
             no: Token<'src>,
         ) -> Token<'src> {
@@ -166,71 +172,83 @@ impl<'src> Iterator for Lexer<'src> {
                 }
             }
         }
+        let f = |s, next, left, right, yes, no| Some(followed_by(s, next, left, right, yes, no));
 
         let mut cs = self.rest().chars();
-        match cs.next()? {
-            '(' => Some(ret_and_adv(self, 1, Token::LeftParen)),
-            ')' => Some(ret_and_adv(self, 1, Token::RightParen)),
-            '{' => Some(ret_and_adv(self, 1, Token::LeftBrace)),
-            '}' => Some(ret_and_adv(self, 1, Token::RightBrace)),
-            '[' => Some(ret_and_adv(self, 1, Token::LeftBracket)),
-            ']' => Some(ret_and_adv(self, 1, Token::RightBracket)),
-            ',' => Some(ret_and_adv(self, 1, Token::Comma)),
-            ':' => Some(ret_and_adv(self, 1, Token::Colon)),
-            ';' => Some(ret_and_adv(self, 1, Token::Semicolon)),
-            '.' => Some(ret_and_adv(self, 1, Token::Dot)),
-            l @ '+' => Some(followed_by(
-                self,
-                l,
-                '=',
-                cs.next(),
-                Token::PlusEqual,
-                Token::Plus,
-            )),
-            l @ '-' => Some(followed_by(
-                self,
-                l,
-                '=',
-                cs.next(),
-                Token::MinusEqual,
-                Token::Minus,
-            )),
-            l @ '*' => Some(followed_by(
-                self,
-                l,
-                '=',
-                cs.next(),
-                Token::StarEqual,
-                Token::Star,
-            )),
-            l @ '/' => Some(followed_by(
-                self,
-                l,
-                '=',
-                cs.next(),
-                Token::SlashEqual,
-                Token::Slash,
-            )),
-            //l @ '%' => Some(followed_by(
-            //    self,
-            //    l,
-            //    '=',
-            //    &mut cs,
-            //    Token::PercentEqual,
-            //    Token::Percent,
-            //)),
-            _ => todo!(), //'-' =>
-                          //'*' => Some(Token::Star),
-                          //'/' => {}
-                          //'~' => Some(Token::Tilde),
-                          //'!' => {}
-                          //'<' => {}
-                          //'>' => {}
-                          //'=' => {}
-                          //'_' => {}
-                          //'&' => {}
+        loop {
+            match cs.next()? {
+                ws if ws.is_whitespace() => {
+                    self.byte_pos += ws.len_utf8();
+                    continue;
+                }
+                '(' => break Some(ret_and_adv(self, '(', T::LeftParen)),
+                ')' => break Some(ret_and_adv(self, ')', T::RightParen)),
+                '{' => break Some(ret_and_adv(self, '{', T::LeftBrace)),
+                '}' => break Some(ret_and_adv(self, '}', T::RightBrace)),
+                '[' => break Some(ret_and_adv(self, '[', T::LeftBracket)),
+                ']' => break Some(ret_and_adv(self, ']', T::RightBracket)),
+                ',' => break Some(ret_and_adv(self, ',', T::Comma)),
+                ':' => break Some(ret_and_adv(self, ':', T::Colon)),
+                ';' => break Some(ret_and_adv(self, ';', T::Semicolon)),
+                '.' => break Some(ret_and_adv(self, '.', T::Dot)),
+                '+' => break f(self, cs.next(), '+', '=', T::PlusEqual, T::Plus),
+                '-' => break f(self, cs.next(), '-', '=', T::MinusEqual, T::Minus),
+                '*' => break f(self, cs.next(), '*', '=', T::StarEqual, T::Star),
+                '/' => break f(self, cs.next(), '/', '=', T::StarEqual, T::Star),
+                '%' => break f(self, cs.next(), '%', '=', T::PercentEqual, T::Percent),
+                '~' => break f(self, cs.next(), '~', '=', T::TildeEqual, T::Tilde),
+                '!' => break f(self, cs.next(), '!', '=', T::BangEqual, T::Bang),
+                '=' => break f(self, cs.next(), '=', '=', T::EqualEqual, T::Equal),
+                '<' => break f(self, cs.next(), '<', '=', T::LessEqual, T::Less),
+                '>' => break f(self, cs.next(), '>', '=', T::GreaterEqual, T::Greater),
+                '&' => break f(self, cs.next(), '&', '&', T::DoubleAmpersand, T::Ampersand),
+                '|' => break f(self, cs.next(), '|', '|', T::DoublePipe, T::Pipe),
+                c if c == '_' || c.is_alphabetic() => {
+                    let start = self.byte_pos;
+                    let mut end = start + c.len_utf8();
+                    while let Some(next) = cs.next()
+                        && (next.is_alphanumeric() || next == '_')
+                    {
+                        end += next.len_utf8()
+                    }
+                    self.byte_pos = end;
+                    let ident = &self.whole[start..end];
+                    break Some(if let Ok(keyword) = Keyword::from_str(ident) {
+                        Token::Keyword(keyword)
+                    } else {
+                        Token::Ident(ident)
+                    });
+                }
+                _ => break None,
+            }
         }
     }
+}
+
+#[test]
+fn basic_lexing() {
+    let s = "x && y;";
+    let mut l = Lexer::new(s);
+    assert_eq!(Token::Ident("x"), l.next().unwrap());
+    assert_eq!(Token::DoubleAmpersand, l.next().unwrap());
+    assert_eq!(Token::Ident("y"), l.next().unwrap());
+    assert_eq!(Token::Semicolon, l.next().unwrap());
+}
+
+#[test]
+fn multichar_ident() {
+    let s = "hola && adeu || (si % no)";
+    let mut l = Lexer::new(s);
+    assert_eq!(Token::Ident("hola"), l.next().unwrap());
+    assert_eq!(Token::DoubleAmpersand, l.next().unwrap());
+    assert_eq!(Token::Ident("adeu"), l.next().unwrap());
+    assert_eq!(Token::DoublePipe, l.next().unwrap());
+
+    assert_eq!(Token::LeftParen, l.next().unwrap());
+    assert_eq!(Token::Ident("si"), l.next().unwrap());
+    assert_eq!(Token::Percent, l.next().unwrap());
+    assert_eq!(Token::Ident("no"), l.next().unwrap());
+    assert_eq!(Token::RightParen, l.next().unwrap());
 }
 
 //l @ '+' => Some(followed_by(
